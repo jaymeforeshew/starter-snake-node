c9eed7efe016d214c49c2760130bb06aa7718133
Jayme Foreshew <jrforeshew@gmail.com>
2019-11-28 15:51:41 -0500

    testing

--- index.js
+++ index.js
@@ -143,13 +143,13 @@ function isMoveSafe(me, move) {
     y -= 1;
   }
 
-  if (board[x][y] != '0' || board[x][y] != 'food') {
+  if (board[x][y] != '0' && board[x][y] != 'food') {
     return false;
   }
 
   return true;
 }
-
+/*
 function survivalMove(me) {
   let x = me.body[0].x;
   let y = me.body[0].y;
@@ -167,7 +167,7 @@ function survivalMove(me) {
   }
 
   return 'down';
-}
+}*/
 
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
@@ -180,7 +180,7 @@ app.post('/move', (request, response) => {
 
   if (!isMoveSafe(request.body.you, nextMove)) {
     // forget the food, just survive!
-    nextMove = survivalMove(request.body.you);
+    //nextMove = survivalMove(request.body.you);
   }
 
   // Response data
